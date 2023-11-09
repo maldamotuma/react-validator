9e61ad9553771c88952e816c6f84b31f4e59afb8
malda.motuma <maldamotuma@gmail.com>
2023-11-09 13:47:44 +0300

    check pusblish

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { useValidator } from '@malda/react-validator';
+import { useValidator } from '@malda/react-validator';
 
-// const TestValidate = () => {
-//     const { validate, valid } = useValidator();
+const TestValidate = () => {
+    const { validate, valid } = useValidator();
 
-//     return (
-//         <>
-//             <p>
-//                 Currently {valid ? "Valid" : "INvalid"}
-//             </p>
-//             <button onClick={validate}>Validate Here</button>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <p>
+                Currently {valid ? "Valid" : "INvalid"}
+            </p>
+            <button onClick={validate}>Validate Here</button>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <div>
-      <h2>Counter with predefined value</h2>
-      {/* <TestValidate /> */}
-    </div>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <div>
+            <h2>Counter with predefined value</h2>
+            <TestValidate />
+        </div>
+    </React.StrictMode>,
 )
